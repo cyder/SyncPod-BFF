1fe450243f5e0454f9ee949cd9c2ee5ef9775e22
Shintani Teppei <teppest1215@gmail.com>
2019-01-31 07:33:25 +0900

    WIP create mock endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,63 @@
 import { ApolloServer, gql } from "apollo-server";
+import * as fs from "fs";
+import * as path from "path";
 
 // The GraphQL schema
 const typeDefs = gql`
+  ${fs.readFileSync(path.resolve(__dirname, "..", "graphql", "types.graphql"), "utf-8")}
+
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    ping: String
+    recentlyJoinedRooms(limit: Int): [Room!]!
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): User
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "world",
+    ping: () => "pong",
+    recentlyJoinedRooms: async (root: any, args: any, context: any, info: any) => {
+      return await [
+        {
+          id: 790,
+          name: "MyString",
+          description: "MyText",
+          key: "rvS5ovc8",
+          public: false,
+          nowPlayingVideo: null, // あとで変更する
+          lastPlayedVideo: null, // あとで変更する
+          onlineUsers: [], // あとで変更する
+        },
+        {
+          id: 928,
+          name: "MyString",
+          description: "MyText",
+          key: "rvS5ovc8",
+          public: true,
+          nowPlayingVideo: null, // あとで変更する
+          lastPlayedVideo: null, // あとで変更する
+          onlineUsers: [], // あとで変更する
+        },
+      ];
+    },
+  },
+  Mutation: {
+    login: async (root: any, args: any, context: any, info: any) => {
+      return await {
+        id: 69,
+        icon: null,
+        email: "user@example.com",
+        name: "MyString",
+        access_token: "69:q1aXhT-onz4sWejxcV1s",
+        created_at: "2018-03-31T07:35:05.000Z",
+        updated_at: "2018-03-31T07:35:05.000Z",
+        self_introduction: null,
+      };
+    },
   },
 };
 
