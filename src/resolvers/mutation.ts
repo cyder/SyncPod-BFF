551a0e91be7a3b5b40503ef0aae065841b83dec6
Shintani Teppei <teppest1215@gmail.com>
2019-04-02 23:19:01 +0900

    sparate Query & Mutation

---
+++ src/resolvers/mutation.ts
@@ -0,0 +1,53 @@
+export default {
+  login: () => ({
+    id: 69,
+    icon: null,
+    email: 'user@example.com',
+    name: 'MyString',
+    accessToken: '69:q1aXhT-onz4sWejxcV1s',
+    createdAt: '2018-03-31T07:35:05.000Z',
+    updatedAt: '2018-03-31T07:35:05.000Z',
+    selfIntroduction: null,
+  }),
+  signup: () => ({
+    id: 94,
+    email: 'user@example.com',
+    name: '',
+    accessToken: '94:U-yJzvseLMqVibMK-y_Z',
+    createdAt: '2018-03-31T07:35:06.000Z',
+    updatedAt: '2018-03-31T07:35:06.000Z',
+    selfIntroduction: null,
+    icon: null,
+  }),
+  createRoom: () => ({
+    id: 1112,
+    name: 'MyString',
+    description: 'MyText',
+    key: 'dAg2r_u9',
+    public: true,
+    nowPlayingVideo: null, // あとで変更する
+    lastPlayedVideo: null, // あとで変更する
+    onlineUsers: [], // あとで変更する
+    createUser: {
+      id: 1988,
+      name: 'MyString',
+      url: null,
+    },
+  }),
+  createUserReport: () => ({
+    id: 1,
+    message: 'message',
+    createdAt: '2018-03-31T07:35:06.000Z',
+    updatedAt: '2018-03-31T07:35:06.000Z',
+  }),
+  uploadUserIcon: () => ({
+    id: 87,
+    accessToken: '87:UyH2nBGrDiyRzsYXa6mx',
+    icon: '/Users/shintanitoshio/Works/YouTubeSyncServer/spec/tmp/uploads/user/icon/87/icon.jpg',
+    email: 'user@example.com',
+    name: 'MyString',
+    createdAt: '2018-03-31T07:35:06.000Z',
+    updatedAt: '2018-03-31T07:35:06.000Z',
+    selfIntroduction: null,
+  }),
+};
